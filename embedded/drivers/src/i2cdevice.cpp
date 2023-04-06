e3f670e4495d4b762ec24454bc2ed2d482b40614
Wigu701 <72235998+Wigu701@users.noreply.github.com>
2023-04-06 17:56:37 -0500

    Restructured I2C helper to be class for easier use. Modified TCA to reflect this

--- embedded/drivers/src/i2cdevice.cpp
+++ embedded/drivers/src/i2cdevice.cpp
@@ -1,4 +1,9 @@
-#include "i2cutil.h"
+#include "i2cdevice.h"
+
+I2CDevice::I2CDevice(I2C* i2cBus, uint8_t addr) {
+    this->i2cBus = i2cBus;
+    this->addr = addr << 1;
+}
 
 /* Reads bytes from the specified register at the specified address
  * It does this by first writing the register offset to the address of the device, then
@@ -6,13 +11,13 @@
  * 
  * return 0 if no errors, 1 if there were errors. 
  */
-int readI2CWrapper(I2C *i2cBus, int addr, uint8_t reg, char buffer[], int numBytes) {
+int I2CDevice::readI2CWrapper(uint8_t reg, char buffer[], int numBytes) {
     char writeBuffer[1] = {reg};
     // check if the I2C write and read throw errors. 
-    if (i2cBus->write(addr, writeBuffer, 1) != 0) {
+    if (this->i2cBus->write(this->addr, writeBuffer, 1) != 0) {
         return 1; 
     }
-    if (i2cBus->read(addr, buffer, numBytes) != 0) {
+    if (this->i2cBus->read(this->addr, buffer, numBytes) != 0) {
         return 1; 
     }
     return 0; // no errors
@@ -24,7 +29,7 @@ int readI2CWrapper(I2C *i2cBus, int addr, uint8_t reg, char buffer[], int numByt
  * 
  * returns 0 if no errors, 1 if there were errors
  */
-int writeI2CWrapper(I2C *i2cBus, int addr, uint8_t reg, char data[], int numBytes) {
+int I2CDevice::writeI2CWrapper(uint8_t reg, char data[], int numBytes) {
     // create a new buffer which is just the input buffer with the register offset prepended.
     char newBuffer[numBytes + 1];
     newBuffer[0] = reg;
@@ -33,7 +38,7 @@ int writeI2CWrapper(I2C *i2cBus, int addr, uint8_t reg, char data[], int numByte
     // }
     memcpy(newBuffer+1, data, numBytes);
     // check if the I2C write throws an error 
-    if(i2cBus->write(addr, newBuffer, numBytes + 1)) {
+    if(this->i2cBus->write(this->addr, newBuffer, numBytes + 1)) {
         return 1;
     }
     return 0; // no errors 
@@ -43,15 +48,15 @@ int writeI2CWrapper(I2C *i2cBus, int addr, uint8_t reg, char data[], int numByte
  * This code was taken from 
  * https://os.mbed.com/users/MACRUM/code/i2cdetect//file/0ca27c666888/main.cpp/
  */
-void i2cdetect(I2C *i2cBus) {
+void I2CDevice::i2cdetect() {
     char buf[10];
     buf[0] = 0;
     printf("\n     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
     for (int adrs = 0; adrs < 0x80; adrs += 16) {
         printf("%02x: ", adrs);
         for (int i = 0; i < 16; i++) {
-            i2cBus->write(((adrs + i) << 1), buf, 1);
-            int ret = i2cBus->read(((adrs + i) << 1), buf, 2);
+            this->i2cBus->write(((adrs + i) << 1), buf, 1);
+            int ret = this->i2cBus->read(((adrs + i) << 1), buf, 2);
             if ((adrs+i) == 0 || (adrs+i) == 1 || (adrs+i) == 2) {
                 printf("   ");
                 continue;
@@ -64,7 +69,7 @@ void i2cdetect(I2C *i2cBus) {
                 printf("-- ");
             }
             if (ret == 0) {
-                printf("%02x ", (adrs + i) << 1);
+                printf("%02x ", (adrs + i));
             }
         }
         printf("\n");
--- main.cpp
+++ main.cpp
@@ -1,30 +1,9 @@
 #include "mbed.h"
 #include "uartApp.h"
-#include "i2cutil.h"
+#include "i2cdevice.h"
 #include "tca6416.h"
 
 int main(void){
-    puts("Program start");
-    puts("Starting tests for TCA driver");
-    puts("Creating I2C bus...");
-    I2C*bus = new I2C(PB_11, PB_10);
-    puts("Done");
-    i2cdetect(bus);
-    puts("Listed I2C Devices. Should appear as 40 and/or 41. Will test 40 for this test");
-    puts("Real address 21, << 1 -> 42");
-    puts("Initializing TCA...");
-    TCA6416* tca = new TCA6416(bus, 0x21);
-    puts("TCA initialized. Proceeding to begin");
-    char testbuffer[1];
-    readI2CWrapper(bus, 0x42, 0x06, testbuffer, 1);
-    puts("done");
-    printf("%d\n",testbuffer[0]);
-    puts("The line above should be 255");
-    puts("Completed raw read testing. Moving on to normal tests...");
-    puts("Setting output and then reading it back");
-    const uint8_t directions[16] = {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0};
-    tca->begin(directions);
-    printf("%d %d\n", tca->get_dir(0, 0), tca->get_dir(0, 1));
 
     if(runUart()) {
         printf("UART application is not running\n");
@@ -32,10 +11,6 @@ int main(void){
     } 
     
     while (1) {
-        printf("%d\n", tca->get_state(1, 0));
-        wait_us(3000000);
-        printf("%d\n", tca->get_state(1, 0));
-        wait_us(3000000);
 
     }
 }
