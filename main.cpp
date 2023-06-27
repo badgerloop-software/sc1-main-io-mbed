9a741336c448ed18a6e899facc805d5a573744f1
James <jamesvollmer02@gmail.com>
2023-06-27 08:16:20 -0500

    Moved BMS TCA code to main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,18 @@ I2C i2cBus(PF_0, PF_1); // (sda, scl)
 DigitalOut pin(PA_7, 1); // this is for the small nucleo so that CAN works.
 Can c(&canBus);
 
+// bms gpio signals
+bool mpi_1;
+bool mpi_2;
+bool mpo_2;
+
 int main(void){
     printf("hey\n");
-	// TODO Check directions and set pins to read to 1 and pins to write to 0
+    // TODO Check directions and set pins to read to 1 and pins to write to 0
     const uint8_t tca_dirs[16] = {0, 1, 1, 1, 1, 1, 1, 1, 
                                     1, 1, 1, 1, 1, 1, 1, 1};
     TCA6416 tca(&i2cBus, 0x20);
+    TCA6416 tca_bms(&i2cBus, 0x21);
     tca.i2cdetect();
     tca.begin(tca_dirs);
 
@@ -62,7 +68,17 @@ int main(void){
         // this has to be set to 1 for the UART app to not get stuck at the fault screen. 
         // it will probably change in the future once software updates the
         // checkRestartEnable() method in DataUnpacker.cpp 
-        set_door(toggle); 
+        // TODO set_door(toggle);
+
+        // bms updateGPIO method
+        mpi_1 = tca_bms->get_state(1, 2); // BMS_MPI1
+        mpi_2 = tca_bms->get_state(1, 3); // BMS_MPI2
+        mpo_2 = tca_bms->get_state(1, 4); // BMS_MPO2
+        set_imd_status(mpo_2);
+        set_charge_enable(tca_bms->get_state(1, 5); // BMS_CHRG_EN
+        set_discharge_enable(tca_bms->get_state(1, 6); // BMS_DSCHRG_EN
+        // update external_eStop
+        set_external_eStop(tca->get_state(1, 5)); // Ext_Estop_Fdbk
     }
     
     printf("Hello, Mbed!\n");
